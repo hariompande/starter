821a2f7a538b0196c86ea18a3fd6f169779fa79e
hariompande <hariomp@zignuts.com>
2024-12-13 19:20:50 +0530

    cypress and jest installation

--- src/app/store/slices/router.store.ts
+++ src/app/store/slices/router.store.ts
@@ -1,6 +1,5 @@
 import { getRouterSelectors } from '@ngrx/router-store';
 import { createSelector } from '@ngrx/store';
-import { get } from 'lodash-es';
 
 /**
  * Router Selectors
@@ -9,5 +8,5 @@ const defaultSelectors = getRouterSelectors();
 
 export const selectors = {
   ...defaultSelectors,
-  selectCurrentPage: createSelector(defaultSelectors.selectRouteData, (data: any) => get(data, 'page', null)),
+  // selectCurrentPage: createSelector(defaultSelectors.selectRouteData, (data: any) => get(data, 'page', null)),
 };
